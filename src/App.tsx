349e5abf3927b4ba381d5b54c6f6e8f1c4a598b1
Miguel Fernandez <miguelfera@gmail.com>
2022-12-28 10:51:10 -0600

    Add Navbar menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
+import NavBar from "@/scenes/navbar";
 
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<string>('home')
   return (
-    <div className="app">
-     app
+    <div className="app bg-gray-20">
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
